fbbd9cb273d110872ede104b5fee3f1fe774a2cc
vishal dukare <dukarevishal08@gmail.com>
2026-02-13 16:27:15 +0530

    i changes 12 to 15

--- blogify/Blogapp/admin.py
+++ blogify/Blogapp/admin.py
@@ -6,7 +6,7 @@ class Blogadmin(admin.ModelAdmin):
     prepopulated_fields = {'slug':('title',)}
     list_display = ('title', 'category', 'author', 'status', 'is_featured')
     search_fields = ('id', 'title', 'category__category_name', 'status')
-    list_display = ('is_featured', )
+    # list_display = ('is_featured', )
     
 
 # Register your models here.
