35aeb0bdf85d0e844665836d273ed96f887e02cc
tanvisasane428 <tanvisasane428@gmail.com>
2026-02-16 20:51:43 +0530

    Posts by category, 16 to 23

--- blogify/blogify/urls.py
+++ blogify/blogify/urls.py
@@ -6,6 +6,6 @@ from django.conf import settings
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',home,name='home'),
-    path('category/',include('Blogapp.urls')),
+    path('category/',include('Blogapp.urls'))
     
 ] + static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
