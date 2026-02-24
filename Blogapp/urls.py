91a89189e40ae35f291e423846f5d1c8033de262
tanvisasane428 <tanvisasane428@gmail.com>
2026-02-24 12:01:32 +0530

    48 to 54 done

--- Blogapp/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from Blogapp.views import *
-
-urlpatterns = [
-    
-    path('<int:category_id>/',posts_by_category,name='posts_by_category')
-   
-] 
--- blogify/dashboard/urls.py
+++ blogify/dashboard/urls.py
@@ -5,5 +5,12 @@ from . import views
 
 urlpatterns = [
     path('',views.dashboard , name='dashboard'),
-    path('categories/',views.categories, name='categories')
+    # Category CRUD
+    path('categories/',views.categories, name='categories'),
+    path('categories/add/',views.add_category , name='add_category'),
+    path('categories/edit/<int:pk>',views.edit_category , name='edit_category'),
+    path('categories/delete/<int:pk>',views.delete_category , name='delete_category'),
+    
+    # Blogs Posts CRUD
+    path('posts/',views.posts, name='posts'),
 ]
